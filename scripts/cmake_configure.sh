50640a6dcc1e18518d14ef13a4791d9552ae8367
Daniel Rahme <daniel.rahme.dr@gmail.com>
2021-09-29 20:50:10 +0200

    added scripts to automate cmake build

---
+++ scripts/cmake_configure.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "CMake configure"
+echo "cmake -S . -B build"
+
+cmake -S . -B build
---
+++ scripts/compile.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Compile"
+
+# Print the command
+echo "cmake --build build"
+
+# This will run the command
+cmake --build build
