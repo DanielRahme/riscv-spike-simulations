7e36a49e03bb1cf4f9a38b4586d93350f4972c2b
Daniel Rahme <daniel.rahme.dr@gmail.com>
2021-10-01 14:52:41 +0200

    added compiler flags

--- scripts/cmake_configure.sh
+++ scripts/cmake_configure.sh
@@ -5,6 +5,8 @@ echo "CMake configure"
 #echo "cmake -S . -B build"
 #cmake -S . -B build
 
-echo "cmake -S . -DCMAKE_TOOLCHAIN_FILE=./cmake/riscv.cmake -B build"
-cmake -S . -DCMAKE_TOOLCHAIN_FILE=./cmake/riscv.cmake -B build
+echo "cmake -S . -DCMAKE_TOOLCHAIN_FILE=./cmake/riscv.cmake -DCMAKE_BUILD_TYPE=Release -B build"
+
+# Build type Release is for -O3 optimization
+cmake -S . -DCMAKE_TOOLCHAIN_FILE=./cmake/riscv.cmake -DCMAKE_BUILD_TYPE=Release -B build
 
