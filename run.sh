14246115629b21ca710ceed4b5dc6c2715116c65
Daniel Rahme <daniel.rahme.dr@gmail.com>
2021-10-12 15:11:28 +0200

    remove unused scripts

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "[1/2] Configure the build"
-./scripts/cmake_configure.sh
-
-echo "[2/2] Build"
-./scripts/cmake_build.sh
-
--- scripts/cmake_build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Compile"
-
-# Print the command
-echo "cmake --build build"
-
-# This will run the command
-cmake --build build
