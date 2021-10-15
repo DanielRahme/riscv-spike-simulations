1f75f003bd82ce08260bac986337a0c5aaf41cdf
Daniel Rahme <daniel.rahme.dr@gmail.com>
2021-10-15 13:31:26 +0200

    spike parser creates bar chart and pie chart using matplot

--- scripts/spike_parser.py
+++ scripts/spike_parser.py
@@ -1,16 +1,15 @@
 # Extract instructions from tracelog generated from Spike.
 # Output the frequency of instructions used. 
+# Create bar chart and pie chart
 import collections
+import matplotlib.pyplot as plt
 
 def extract_instructions(filename):
-
     instructs = []
 
     with open(filename) as f:
         for line in f:
             instructs.append(line.split()[4])
-    #c = collections.Counter(instructs)
-    #print(dict(c))
     return collections.Counter(instructs)
 
 def write_to_csv(freq_instructs, outfile):
@@ -22,10 +21,39 @@ def write_to_csv(freq_instructs, outfile):
 
     print ("Created file " + output_filename)
 
+def print_bar_chart(filepath, data):
+    names = list(data.keys())
+    values = list(data.values())
 
-def main(filename, output_name):
-    freq_instructs = extract_instructions(filename)
-    write_to_csv(dict(freq_instructs), output_name)
+    fig, ax = plt.subplots()
+    ax.bar(names, values)
+
+    fig.suptitle('Instruction frequency')
+    plt.xticks(rotation=90)
+    ax.set_ylabel('Freq')
+    ax.set_xlabel('Instruction')
+
+    bar_chart_file = filepath + "_barchart.png"
+    plt.savefig(bar_chart_file)
+    #plt.show()
+
+def print_pie_chart(filepath, data):
+    labels = list(data.keys())
+    sizes = list(data.values())
+
+    fig, ax = plt.subplots()
+    ax.pie(sizes, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90)
+    ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+    pie_chart_file = filepath + "_piechart.png"
+    plt.savefig(pie_chart_file)
+    #plt.show()
+
+def main(input_file, output_path):
+    freq_instructs = dict(extract_instructions(input_file))
+    write_to_csv(freq_instructs, output_path)
+    print_bar_chart(output_path, freq_instructs)
+    print_pie_chart(output_path, freq_instructs)
 
 if __name__ == "__main__":
     import sys
