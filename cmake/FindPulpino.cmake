a3d63f381af900d2f3f114d485d9c118ee687a6a
Daniel Rahme <daniel.rahme.dr@gmail.com>
2021-10-25 15:43:01 +0200

    move cmake modules to own directory

--- cmake/FindPulpino.cmake
+++
@@ -1,9 +0,0 @@
-### CMakeLists.txt for external libraries
-include(FetchContent)
-
-FetchContent_Declare(   
-    pulpino-cmsis-dsp   
-    GIT_REPOSITORY https://github.com/misaleh/pulpino
-    SOURCE_SUBDIR  cmake
-    )
-FetchContent_MakeAvailable(pulpino-cmsis-dsp)
