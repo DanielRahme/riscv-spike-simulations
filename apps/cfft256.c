12e81442335c9f59e554cb57ffc910a5be757cdc
Daniel Rahme <daniel.rahme.dr@gmail.com>
2021-11-18 14:17:58 +0100

    created cfft cases from 64 to 4096 fft-points

--- apps/cfft256.c
+++ apps/cfft256.c
@@ -1,16 +1,17 @@
+
 #include <stdlib.h>
 #include <math.h>
 #include <riscv_dsp/riscv_math.h>
 #include <riscv_dsp/riscv_const_structs.h>
 #include <fft_input_array.h>
 
-uint32_t fftSize = 64;
+uint32_t fftSize = 256;
 uint32_t ifftFlag = 0;
 uint32_t doBitReverse = 1;
 
 int main()
 {
-    riscv_cfft_f32(&riscv_cfft_sR_f32_len64, input_f32_fft_64, ifftFlag, doBitReverse);
+    riscv_cfft_f32(&riscv_cfft_sR_f32_len256, input_f32_fft_256, ifftFlag, doBitReverse);
 
     return 0;
 }
