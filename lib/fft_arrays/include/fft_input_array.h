25702815614d9df4233dde843c504fc4bf00cf1d
Daniel Rahme <daniel.rahme.dr@gmail.com>
2021-12-01 16:17:47 +0100

    update input fft arrays

--- lib/fft_arrays/include/fft_input_array.h
+++ lib/fft_arrays/include/fft_input_array.h
@@ -1,7 +1,6 @@
 #ifndef FFT_INPUT_ARRAY_H
 #define FFT_INPUT_ARRAY_H
-
-
+#include <stdint.h>
 extern float input_f32_fft_64[128];
 extern float input_f32_fft_128[256];
 extern float input_f32_fft_256[512];
@@ -10,5 +9,24 @@ extern float input_f32_fft_1024[2048];
 extern float input_f32_fft_2048[4096];
 extern float input_f32_fft_4096[8192];
 extern float input_f32_fft_8192[16384];
+
+extern int16_t input_q15_fft_64[128];
+extern int16_t input_q15_fft_128[256];
+extern int16_t input_q15_fft_256[512];
+extern int16_t input_q15_fft_512[1024];
+extern int16_t input_q15_fft_1024[2048];
+extern int16_t input_q15_fft_2048[4096];
+extern int16_t input_q15_fft_4096[8192];
+extern int16_t input_q15_fft_8192[16384];
+
+extern int32_t input_q31_fft_64[128];
+extern int32_t input_q31_fft_128[256];
+extern int32_t input_q31_fft_256[512];
+extern int32_t input_q31_fft_512[1024];
+extern int32_t input_q31_fft_1024[2048];
+extern int32_t input_q31_fft_2048[4096];
+extern int32_t input_q31_fft_4096[8192];
+extern int32_t input_q31_fft_8192[16384];
+
 
 #endif
