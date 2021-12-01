075a672ff70892b417363b76469e49dec58e984f
Daniel Rahme <daniel.rahme.dr@gmail.com>
2021-12-01 18:11:38 +0100

    added q15 rfft test cases

---
+++ apps/rfft_q15_64.c
@@ -0,0 +1,25 @@
+
+#include <stdlib.h>
+#include <math.h>
+#include <riscv_dsp/riscv_math.h>
+#include <riscv_dsp/riscv_const_structs.h>
+#include <fft_input_array.h>
+
+#define FFT_SIZE 64
+#define INPUT_LENGTH 2*FFT_SIZE
+
+uint32_t fftSize = FFT_SIZE;
+uint32_t ifftFlag = 0;
+uint32_t doBitReverse = 0;
+
+static riscv_rfft_instance_q15 S;
+static q15_t output[INPUT_LENGTH];
+
+
+int main()
+{
+    riscv_rfft_init_q15(&S, fftSize, ifftFlag, doBitReverse);
+    riscv_rfft_q15(&S, input_q15_fft_64, output);
+
+    return 0;
+}
