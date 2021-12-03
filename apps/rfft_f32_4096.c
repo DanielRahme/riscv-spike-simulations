436dd4806e1463e34c78e70cd3ba8bbf8cb28cdc
Daniel Rahme <daniel.rahme.dr@gmail.com>
2021-12-03 13:54:01 +0100

    added rfft f32 test cases

---
+++ apps/rfft_f32_4096.c
@@ -0,0 +1,25 @@
+
+#include <stdlib.h>
+#include <math.h>
+#include <riscv_dsp/riscv_math.h>
+#include <riscv_dsp/riscv_const_structs.h>
+#include <fft_input_array.h>
+
+#define FFT_SIZE 4096
+#define INPUT_LENGTH 2*FFT_SIZE
+
+uint32_t fftSize = FFT_SIZE;
+uint32_t ifftFlag = 0;
+uint32_t doBitReverse = 0;
+
+static riscv_rfft_fast_instance_f32 S;
+static float output[INPUT_LENGTH];
+
+int main()
+{
+    riscv_rfft_fast_init_f32(&S, FFT_SIZE);
+    riscv_rfft_fast_f32(&S, input_f32_fft_4096, output, ifftFlag);
+
+    return 0;
+}
+
