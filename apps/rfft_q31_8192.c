801ff624a7bf26200638643f07dc14d1dca37403
Daniel Rahme <daniel.rahme.dr@gmail.com>
2021-12-08 18:14:13 +0100

    added rfft 8192-point case for q31 and q15

---
+++ apps/rfft_q31_8192.c
@@ -0,0 +1,27 @@
+
+#include <stdlib.h>
+#include <math.h>
+#include <riscv_dsp/riscv_math.h>
+#include <riscv_dsp/riscv_const_structs.h>
+#include <fft_input_array.h>
+
+#define FFT_SIZE 8192
+#define INPUT_LENGTH 2*FFT_SIZE
+
+uint32_t fftSize = FFT_SIZE;
+uint32_t ifftFlag = 0;
+uint32_t doBitReverse = 0;
+
+static riscv_rfft_instance_q31 S;
+static q31_t output[INPUT_LENGTH];
+
+
+int main()
+{
+    riscv_rfft_init_q31(&S, fftSize, ifftFlag, doBitReverse);
+    riscv_rfft_q31(&S, input_q31_fft_8192, output);
+
+    return 0;
+}
+
+
