6de88a61fcc24d19b45158f48e58ba30d83a43c8
Daniel Rahme <daniel.rahme.dr@gmail.com>
2021-12-10 14:06:45 +0100

    update horizontal barchart with categories

--- scripts/calc_cycles.py
+++ scripts/calc_cycles.py
@@ -90,74 +90,237 @@ def print_bar_chart(file_prefix, data):
     plt.savefig(bar_chart_file, dpi=300)
 
 def print_pie_chart(file_prefix, data):
-    count_lsm = 0
-    count_as = 0
-    count_mac = 0
-    count_muldiv = 0
+    count_load = 0
+    count_store = 0
+    count_add = 0
+    count_fadd = 0
+    count_sub = 0
+    count_fsub = 0
+    count_addi = 0
+    count_move = 0
+    count_fmove = 0
+    count_fmac = 0
+    count_mult_div = 0
+    count_fmult_div = 0
     count_branch = 0
     count_logical = 0
-    count_app = 0
     count_other = 0
-    count_not_def = 0
 
     ## The categories
-    load_store_move = ["c.sw","c.flwsp","c.fswsp","li","c.swsp","c.lwsp", "c.flw", "c.fsw", "c.lw",
-            "c.mv", "c.li", "c.lui","lui", "lb", "lh", "lw", "lbu", "lhu", "sb", "sh", "sw", "flw",
-            "fsw", "fmv.x.w", "fmv.w.x", "mv" ]
-
-    add_sub = ["c.add","c.addi16sp", "auipc", "c.addi","addi", "add","c.sub", "sub", "fadd.s",
-            "fsub.s", "c.addi4spn" ]
-    mac = ["fmadd.s", "fmsub.s", "fnmsub.s", "fnmadd.s"]
-
-    mult_div = ["mul","mulh","mulhsu","mulhu","div","divu","rem","remu","fmul.s","fdiv.s"]
-
-    branch = ["c.bnz","c.j","beqz","c.jal","jal", "jalr", "beq", "bne", "blt", "bge", "bltu",
-            "bgeu", "c.jalr", "c.jr", "c.beqz", "bnez", "c.bnez"
+    cat_load = [
+            "flw",
+            "c.flw",
+            "c.flwsp",
+            "c.lwsp",
+            "c.lw",
+            "lb",
+            "lh",
+            "lw",
+            "lbu",
+            "lhu"
             ]
-    logical = ["slti","sltiu","xori","ori","andi","c.slli","slli","srli","srai","sll","slt","sltu"
-            , "xor","srl","sra","or","and","feq.s","flt.s","fle.s", "c.or", "c.and", "c.xor",
-            "c.andi", "c.srai", "c.srli", "snez" ] 
 
-    application = ["ret","fence", "ecall", "ebreak", "c.ebreak", "c.nop" ]
+    cat_store = [
+            "c.sw",
+            "c.fsw",
+            "c.fswsp",
+            "c.swsp",
+            "sb",
+            "sh",
+            "sw",
+            "fsw"
+            ]
 
-    other = ["fsqrt.s","fsgnj.s","fsgnjn.s","fsgnjx.s","fmin.s","fmax.s","fcvt.w.s",
-            "fcvt.wu.s","fclass.s","fcvt.s.w","fcvt.s.wu"] 
+    cat_add = [
+            "c.mv",
+            "c.add",
+            "add"
+    ]
+    cat_fadd = [
+            "fadd.s",
+            ]
+
+    cat_sub = [
+            "c.sub",
+            "sub",
+            ]
+
+    cat_fsub = [
+            "fsub.s",
+            ]
+
+    cat_addi = [
+            "c.li",
+            "li",
+            "mv",
+            "c.addi",
+            "addi",
+            "c.addi16sp",
+            "auipc",
+            "c.addi4spn" 
+            ]
+
+
+    cat_move = [
+            "lui",
+            "c.lui"
+            ]
+    cat_fmove = [
+            "fmv.x.w",
+            "fmv.w.x"
+            ]
+
+
+    cat_fmac = [
+            "fmadd.s",
+            "fmsub.s",
+            "fnmsub.s",
+            "fnmadd.s"
+            ]
+
+    cat_mult_div = [
+            "mul",
+            "mulh",
+            "mulhsu",
+            "mulhu",
+            "div",
+            "divu",
+            "rem",
+            "remu"
+            ]
+
+    cat_fmult_div = [
+            "fmul.s",
+            "fdiv.s"
+            ]
+
+    cat_branch = ["c.bnz",
+            "c.j",
+            "beqz",
+            "c.jal",
+            "jal",
+            "jalr",
+            "beq",
+            "bne",
+            "blt",
+            "bge",
+            "bltu",
+            "bgeu",
+            "c.jalr",
+            "c.jr",
+            "c.beqz",
+            "bnez",
+            "c.bnez"
+            ]
+    cat_logical = ["slti",
+            "sltiu",
+            "xori",
+            "ori",
+            "andi",
+            "c.slli",
+            "slli",
+            "srli",
+            "srai",
+            "sll",
+            "slt",
+            "sltu",
+            "xor",
+            "srl",
+            "sra",
+            "or",
+            "and",
+            "feq.s",
+            "flt.s",
+            "fle.s",
+            "c.or",
+            "c.and",
+            "c.xor",
+            "c.andi",
+            "c.srai",
+            "c.srli",
+            "snez" 
+            ] 
+
+    cat_other = ["fsqrt.s",
+            "fsgnj.s",
+            "fsgnjn.s",
+            "fsgnjx.s",
+            "fmin.s",
+            "fmax.s",
+            "fcvt.w.s",
+            "fcvt.wu.s",
+            "fclass.s",
+            "fcvt.s.w",
+            "fcvt.s.wu",
+            "ret",
+            "fence",
+            "ecall",
+            "ebreak",
+            "c.ebreak",
+            "c.nop" 
+            ]
 
     for inst, count in data.items():
-        # Single cycle instructions
-        if inst in load_store_move:
-            count_lsm += count
-        elif inst in add_sub:
-            count_as += count
-        elif inst in mac:
-            count_mac += count
-        elif inst in mult_div:
-            count_muldiv += count
-        elif inst in branch:
+        if inst in cat_load:
+            count_load += count
+        elif inst in cat_store:
+            count_store += count
+        elif inst in cat_add:
+            count_add += count
+        elif inst in cat_fadd:
+            count_fadd += count
+        elif inst in cat_sub:
+            count_sub += count
+        elif inst in cat_fsub:
+            count_fsub += count
+        elif inst in cat_addi:
+            count_addi += count
+        elif inst in cat_move:
+            count_move += count
+        elif inst in cat_fmove:
+            count_fmove += count
+        elif inst in cat_fmac:
+            count_fmac += count
+        elif inst in cat_mult_div:
+            count_mult_div += count
+        elif inst in cat_fmult_div:
+            count_fmult_div += count
+        elif inst in cat_branch:
             count_branch += count
-        elif inst in logical:
+        elif inst in cat_logical:
             count_logical += count
-        elif inst in application:
-            count_app += count
-        elif inst in other:
+        elif inst in cat_other:
             count_other += count
         else:
-            count_not_def += count
-            print("Instruction <" + inst + "> is not defined in a bin")
+            try:
+                raise NameError('Instruction not in a category')
+            except NameError:
+                #print('Not defined instruction found.')
+                raise
 
 
-    labels = ["load/store/move", "add/sub", "mac", "mult/div", "branch", "logical", "application",
-            "other", "not defined"]
+    labels = [
+        "load", "store", "add" , "fadd", "sub",
+        "fsub" , "addi", "move", "fmove", "fmac",
+        "mult_div", "fmult_div", "branch", "logical", "other"
+            ]
 
-    categories = [count_lsm, count_as, count_mac, count_muldiv, count_branch, count_logical, count_app, count_other, count_not_def]
+    categories = [ 
+            count_load, count_store, count_add, 
+            count_fadd, count_sub, count_fsub, 
+            count_addi, count_move, count_fmove,
+            count_fmac, count_mult_div, count_fmult_div,
+            count_branch, count_logical, count_other
+            ]
 
     tot = sum(categories)
     percent = [100 * x/tot for x in categories]
     y_pos = np.arange(len(labels))
 
+    ### Plot results
     fig, ax = plt.subplots()
-    plt.grid(axis = 'x')
-    ax.barh(y_pos, percent, align='center', height=0.4)
+    ax.grid(axis='x', zorder=0)
+    ax.barh(y_pos, percent, align='center', height=0.4, zorder=3)
     ax.set_yticks(y_pos, labels=labels)
     ax.invert_yaxis()  # labels read top-to-bottom
     ax.set_xlabel('Percent %')
@@ -166,6 +329,8 @@ def print_pie_chart(file_prefix, data):
     for index, value in enumerate(percent):
         plt.text(value, index, str("{:.2f}".format(value)))
 
+    case_name = os.path.basename(file_prefix).removesuffix("-freq-instruct.csv")
+    fig.suptitle(case_name)
     pie_chart_file = file_prefix + "-piechart.png"
     plt.savefig(pie_chart_file, dpi=300)
 
