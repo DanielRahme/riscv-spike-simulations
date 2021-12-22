a22e3e48bfc017714fe87ecac3b8b42aca8ce467
Daniel Rahme <daniel.rahme.dr@gmail.com>
2021-12-22 19:02:12 +0100

    make spike_parser redirect input

--- scripts/spike_parser.py
+++ scripts/spike_parser.py
@@ -1,29 +1,28 @@
 # Extract instructions from tracelog generated from Spike.
 # Output the frequency of instructions used. 
 # Create bar chart and pie chart
+import sys
 import collections
 
-def extract_instructions(filename):
+def extract_instructions():
     exception_found = False
     instructs = []
 
-    with open(filename) as f:
-        for line in f:
-            words = line.split()
+    for line in sys.stdin:
+        words = line.split()
+        if (words[2] != "exception" and not(exception_found)):
+            instructs.append(words[4])
 
-            if (words[2] != "exception" and not(exception_found)):
-                instructs.append(words[4])
+        elif (words[2] == "exception"):
+            exception_found = True
 
-            elif (words[2] == "exception"):
-                exception_found = True
+        # Handles the tval instruction
+        elif (len(words) < 5 and exception_found):  
+            pass
 
-            # Handles the tval instruction
-            elif (len(words) < 5 and exception_found):  
-                pass
-
-            elif (exception_found):
-                if (words[4] == "sret"):
-                    exception_found = False
+        elif (exception_found):
+            if (words[4] == "sret"):
+                exception_found = False
 
     return collections.Counter(instructs)
 
@@ -32,10 +31,10 @@ def print_results(freq_instructs):
         print(op + "," + str(count))
 
 
-def main(input_file):
-    freq_instructs = dict(extract_instructions(input_file))
+def main():
+    freq_instructs = dict(extract_instructions())
     print_results(freq_instructs)
 
 if __name__ == "__main__":
     import sys
-    main(sys.argv[1])
+    main()
